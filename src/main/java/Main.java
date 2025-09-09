18d71df8b0ddf156bd758f3a705a1afc87a4c363
Meraj Hossain Mukto <mukto@Merajs-MacBook-Air.local>
2025-09-09 17:57:45 +0600

    project initialized

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        driver.quit();
+    }
+}
