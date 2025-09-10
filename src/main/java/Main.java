b5b3f5ea6cedffdc926eef37d73d8042c61c6df9
Meraj Hossain Mukto <mukto@Merajs-MacBook-Air.local>
2025-09-10 15:03:13 +0600

    dependencies and some test added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,28 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        driver.manage().window().maximize();
+//        driver.get("https://www.google.com");
+//        String url = driver.getCurrentUrl();
+//        String title = driver.getTitle();
+//
+//        System.out.println(url);
+//        System.out.println(title);
+
+//        driver.navigate().to("https://www.google.com/advanced_search");
+//        driver.navigate().back();
+//        driver.navigate().refresh();
+//        driver.navigate().forward();
+
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.cssSelector(".app_logo")).isDisplayed();
+
         driver.quit();
     }
 }
